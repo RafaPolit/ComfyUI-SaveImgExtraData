a8d93696ebc1c6d31addf04c8e3bfb0bd57e0919
RafaPolit <rafaelpolit@gmail.com>
2024-09-02 16:39:47 -0500

    Changed location of prompt

--- __init__.py
+++ __init__.py
@@ -3,12 +3,12 @@ import folder_paths
 import os, sys, subprocess
 
 try:
-  print("### Loading: Save img prompt")
-  from .Save_img_prompt import NODE_CLASS_MAPPINGS
+  print("### Loading: Save img extra data")
+  from .Save_img_extra_data import NODE_CLASS_MAPPINGS
 except:
   img_path = os.path.dirname(__file__)
-  from .Save_img_prompt import NODE_CLASS_MAPPINGS
+  from .Save_img_extra_data import NODE_CLASS_MAPPINGS
 
-comfy_path = os.path.dirname(folder_paths.__file__)           
+comfy_path = os.path.dirname(folder_paths.__file__)
 
 __all__ = ['NODE_CLASS_MAPPINGS']
